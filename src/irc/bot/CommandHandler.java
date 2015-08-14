3b76c85061891ecbb25aa1114e10e9c5d6c658f8
jeffry1829 <jeffry1829@gmail.com>
2015-08-14 14:30:26 +0800

    add feature: hook == cancelable

--- src/irc/bot/CommandHandler.java
+++ src/irc/bot/CommandHandler.java
@@ -6,7 +6,8 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class CommandHandler implements Runnable{
-	
+	//其實算是CommandEvent
+	//不過不使用Event的語法banfile
 	Socket server;
 	BufferedReader reader;
 	
@@ -19,16 +20,17 @@ public class CommandHandler implements Runnable{
 	public void run() {
 		String msline;
 		String username;
+		
 		try{
 			while( ( msline=reader.readLine() ) != null){
 			
-				if( msline.matches(".* PRIVMSG .*") ){
+				if( msline.matches("\bPRIVMSG\b") ){
 					username=new String(msline).substring(msline.indexOf("#" , 2) , msline.indexOf(":" , 2));
 					String[] arr=msline.split(":*:");
 					msline=msline.substring(arr[0].length());
-					if(msline.startsWith("\\")){ //prefix setting
+					if( msline.startsWith("\\") ){ //prefix setting還有event開始了.
 						for(CommandExecutor command : JIRCBOTPlugin.commandlist){
-							command.onCommand(username , "\\" , command.getName() , msline.replaceFirst("\\" , "").split(" "));
+							new UserCommandProcessEvent(username , "\\" , command.getName() , msline.replaceFirst("\\" , "").split(" ") , command).Do();
 						}
 					}
 				}
@@ -38,10 +40,9 @@ public class CommandHandler implements Runnable{
 		catch(Exception e){
 			e.printStackTrace();
 		}
-	}
-		
+	
+	}	
 }
-
 
 
 
--- src/irc/bot/reciveMessageEvent.java
+++ src/irc/bot/reciveMessageEvent.java
@@ -3,8 +3,6 @@ package irc.bot;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.Socket;
 
 public class reciveMessageEvent implements CustomEvent,Runnable{
@@ -26,19 +24,8 @@ public class reciveMessageEvent implements CustomEvent,Runnable{
 	@Override
 	public void Do() {
 		
-		for(  JIRCBOTListener event :  JIRCBOTPlugin.jircbotlistenerlist ){
-			Method[] methods=event.getClass().getMethods();
-			for( Method method : methods ){
-				if( method.getParameterTypes().equals(this) ){ //為什麼沒有contains?
-					try {
-						method.invoke(this); //觸發了自己寫的Method
-					} catch (IllegalAccessException | IllegalArgumentException
-							| InvocationTargetException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
+		//invoke overrided event
+		staticMethod.invokeOverrideEvent(this);
 		
 	}
 	
